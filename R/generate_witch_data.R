09d30501dd88986db6caf16f209dbed63a9246ae
Laurent Drouet <ldrouet@gmail.com>
2019-06-26 16:16:38 +0200

    Initial commit

---
+++ R/generate_witch_data.R
@@ -0,0 +1,15 @@
+# Translate witch data
+#
+# This function translates WITCH data using a set of make_data files (R or gams).
+#
+# -o <output_dir>  Path to save the translated gdx (default: data_test)
+# -n <region>      Region mapping, as in regions/*.inc (default: witch17)
+# -t <time>        Time mapping (default: t30)
+# -g <gdxfiles>    Specific gdx files/pattern to convert (default: input/build/data*)
+# -d <data_dir> Path to the data folder (default: ../witch-data)'
+
+generate_witch_data <- function(output_dir, region, time, gdxfiles, witch_data_dir) {
+
+  print("Hello, world!")
+
+}
