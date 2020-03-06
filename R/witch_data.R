600da17ebb8ccde826332a1c177999f235163545
Laurent Drouet <ldrouet@gmail.com>
2020-03-06 16:04:52 +0100

    add witch_data_upload

--- R/witch_data.R
+++ R/witch_data.R
@@ -2,7 +2,7 @@
 #' after downloading it from github if necessary using the piggyback library.
 #' @param file Name of the file in the with-data repository
 #' @param version Release version of the file (required for method piggyback)
-#' @param method 'piggyback' or 'repository'.
+#' @param method 'piggyback' or 'witch-data'.
 #' @param noCheck For the piggyback method, don't check and download the file.
 #' @param repo github repository name
 #'
@@ -46,5 +46,37 @@ witch_data <- function(file, version = NULL,
   }
 
   return(file.path(idir,file))
+
+}
+
+
+#' Upload a file as an asset in a release of a github repository.
+#' @param file Name of the file in the with-data repository
+#' @param version Release version of the file (without space)
+#' @param method 'piggyback'.
+#' @param repo github repository name
+#'
+#' @export
+#' @examples
+#' \dontrun{
+#' witch_data_upload("ssp/ssp_population.csv", version = 'v0.0.1')
+#' }
+witch_data_upload <- function(file, version = NULL,
+                              method = getOption('witchtools.method'),
+                              repo = getOption('witchtools.witch_data_repo')) {
+
+  # Check method name
+  if (!method %in% c("piggyback","witch-data")) {
+    warning(paste("Method", method, "does not exist."))
+  }
+
+  if (method == "piggyback") {
+
+    if (is.null(version)) stop('version cannot be NULL')
+
+    try(piggyback::pb_new_release(repo = repo, tag = version), silent = TRUE)
+    piggyback::pb_upload(file, name = stringr::str_replace_all(file,"/","-"), tag = version)
+
+  }
 
 }
