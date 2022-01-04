13989e08e88197faee9bd74ca2c0cb2d31af3e6f
Laurent Drouet <ldrouet@pm.me>
2022-01-04 11:55:09 +0000

    clean data-raw

--- data-raw/time_mappings.R
+++ data-raw/time_mappings.R
@@ -2,27 +2,6 @@
 
 library(data.table)
 
-load_timescale_mapping <- function(f) {
-  tab <- data.table::fread(f, colClasses = "character")
-  data.table::setnames(tab, "year", "refyear")
-  # Expand year
-  tab <- rbindlist(lapply(
-    seq_len(nrow(tab)),
-    function(i) {
-      tab[i, .(t,
-        year = begyear:endyear,
-        refyear,
-        pred,
-        tperiod,
-        begyear,
-        endyear
-      )]
-    }
-  ))
-  tab[, year := as.numeric(year)]
-  return(tab)
-}
-
 # Timescale mappings
 time_mapping_files <- Sys.glob(file.path("data-raw", "timescale", "*.csv"))
 time_mappings <- lapply(time_mapping_files, witchtools::witch_time_mapping)
