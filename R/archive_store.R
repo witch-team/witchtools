519315a1e80d04c1d68f11381567027053cb1d32
Laurent Drouet <ldrouet@pm.me>
2023-11-21 21:08:24 +0100

    fix zeus overwrite

--- R/archive_store.R
+++ R/archive_store.R
@@ -9,7 +9,10 @@ archive_store <- function(filename, dir_list) {
   # Copy file in the archive
   cp_file <- function(folder, .f) {
     if (dir.exists(folder)) {
-      fs::file_copy(.f, folder, overwrite = TRUE)
+      if (fs::file_exists(fs::path(folder,.f))) {
+        fs::file_delete(fs::path(folder,.f))
+      }
+      fs::file_copy(.f, folder)
       cat(paste("Copied file to", folder, "\n"))
     }
   }
