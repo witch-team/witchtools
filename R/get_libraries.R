2a060f76304123e2987a7e19656e362d5724c2ac
Laurent Drouet <ldrouet@pm.me>
2025-11-02 20:31:58 +0000

    🔧 chore: Simplify package installation process
    - Simplify package installation by removing conditional checks for interactive mode.

--- R/get_libraries.R
+++ R/get_libraries.R
@@ -156,15 +156,11 @@ install_duckdb_safe <- function() {
   message("Installing package '", pkg, "' from r-universe and CRAN...")
 
   print(interactive())
+  utils::install.packages(pkg, repos = repos)
 
   # Attempt installation
   install_result <- tryCatch({
-    if (interactive()) {
-      utils::install.packages(pkg, quiet = FALSE, repos = repos)
-    } else {
-      # Non-interactive: install without prompting
-      utils::install.packages(pkg, quiet = FALSE, repos = repos)
-    }
+    utils::install.packages(pkg, repos = repos)
     TRUE
   }, error = function(e) {
     list(error = TRUE, message = conditionMessage(e))
