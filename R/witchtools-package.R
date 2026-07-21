730a7f54238516cbcb6e680675b7f7ef46e66464
Laurent Drouet <ldrouet@pm.me>
2026-07-21 12:54:43 +0100

    refactor: preserve user options in .onLoad and add package doc
    .onLoad() now sets the witchtools.* options only when the user has not
    already set them, instead of unconditionally overriding them. Add a
    standard R/witchtools-package.R with the "_PACKAGE" sentinel as the home
    for the package-level @importFrom, and update globalVariables() to cover
    the data.table NSE column names and the datasets referenced by name.

---
+++ R/witchtools-package.R
@@ -0,0 +1,7 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom data.table ":=" "%like%" "%between%"
+## usethis namespace: end
+NULL
--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,19 @@
 # zzz.R
-.onLoad <- function(libname, pkgname) {
-  options(witchtools.method = "local")
-  options(witchtools.witch_data_repo = "witch-team/witch-data")
-  options(witchtools.noCheck = TRUE)
-}
 
-# Import package operators
-#' @importFrom data.table ":=" "%like%" "%between%"
+# Set package options, without clobbering any the user has already set.
+.onLoad <- function(libname, pkgname) {
+  op <- options()
+  op_witchtools <- list(
+    witchtools.method = "local",
+    witchtools.witch_data_repo = "witch-team/witch-data",
+    witchtools.noCheck = TRUE
+  )
+  toset <- !(names(op_witchtools) %in% names(op))
+  if (any(toset)) {
+    options(op_witchtools[toset])
+  }
+  invisible()
+}
 
 # Make sure data.table knows we know we're using it
 .datatable.aware <- TRUE
@@ -14,5 +21,18 @@
 # Prevent R CMD check from complaining about the use of pipe expressions
 # standard data.table variables
 if (getRversion() >= "2.15.1") {
-  utils::globalVariables(c(".", ".I", ".N", ".SD"), utils::packageName())
+  utils::globalVariables(
+    c(
+      # data.table special symbols
+      ".", ".I", ".N", ".SD",
+      # column names used in data.table non-standard evaluation
+      "begyear", "description", "endyear", "gdx", "iso3",
+      "n", "pred", "reg", "refyear", "scenario", "tperiod",
+      "V1", "V2", "value", "weight", "ww", "year",
+      # package datasets referenced by name
+      "default_weights", "region_descriptions", "region_mappings",
+      "time_mappings", "witch_meta_param", "witch_period_year"
+    ),
+    utils::packageName()
+  )
 }
